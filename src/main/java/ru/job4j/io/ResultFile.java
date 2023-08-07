e63cb45055c5dd63643821b37d8c14cd46aa1314
ArcueidJungle <kaminoonte00@gmail.com>
2023-08-07 06:10:48 +0300

    2.0. FileOutputStream [#252488  [#252488]]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static int[][] multiple(int size) {
+        int[][] array = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                array[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
+            int[][] matrix = multiple(10);
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    String result = matrix[i][j] + " ";
+                    out.write(result.getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
